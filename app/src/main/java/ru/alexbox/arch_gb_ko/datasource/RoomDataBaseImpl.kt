66b3fe97db19a9a0a8dadb89e5d302e2613eaebe
Alex Podgurski <alexbox726@gmail.com>
2020-12-04 16:58:14 +0200

    layout optimized

--- app/src/main/java/ru/alexbox/arch_gb_ko/datasource/RoomDataBaseImpl.kt
+++ app/src/main/java/ru/alexbox/arch_gb_ko/datasource/RoomDataBaseImpl.kt
@@ -8,6 +8,6 @@ import ru.alexbox.arch_gb_ko.data.SearchResult
 class RoomDataBaseImpl : IDataSource<List<SearchResult>> {
 
     override fun getData(word: String): Observable<List<SearchResult>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented")
     }
 }
